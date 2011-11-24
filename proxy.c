d4019dc552e0f990b2dddbe14ff85e80b849508e
Prashant Sridhar <psridha1@andrew.cmu.edu>
2011-11-23 22:14:57 -0500

    first commit

---
+++ proxy.c
@@ -0,0 +1,13 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "csapp.h"
+
+int main (int argc, char *argv [])
+{
+    printf ("Hi! I am a proxy. But I do nothing.\n");
+    return 0;
+}
+
