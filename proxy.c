39d6fd408202ab4d4897de413442be1a3ed860bf
Prashant Sridhar <psridha1@andrew.cmu.edu>
2011-12-02 21:21:53 -0500

    started proxy

--- proxy.c
+++ proxy.c
@@ -5,9 +5,28 @@
 #include <string.h>
 #include "csapp.h"
 
-int main (int argc, char *argv [])
-{
-    printf ("Hi! I am a proxy. But I do nothing.\n");
-    return 0;
+void fetchRequest(int connfd);
+
+int main (int argc, char *argv []){
+	
+	int listenfd, connfd, port, clientlen;
+	struct sockaddr_in clientaddr;
+	
+	if(argc != 2){
+		fprintf(stderr,"ussage %s <port>\n",argv[0]);
+		exit(1);
+	}
+	port = atoi(argv[1]);
+	listenfd = Open_listenfd(port);
+	while(1) {
+		clientlen = sizeof(clientaddr);
+		connfd = Accept(listenfd , (SA *)&clientaddr, &clientlen);
+		fetchRequest(connfd);
+		Close(connfd);
+}
+	
+void fetchRequest(int connfd){
+	size_t n;
+	char buf
 }
 
