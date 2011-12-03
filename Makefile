57e3a8f805c670f2431b56868ebb2ba8623c566a
Jeff Cooper <weirdsox11@gmail.com>
2011-12-03 00:34:22 -0500

    temporary commit

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -g -Wall
+CFLAGS = -g -Werror -pedantic -std=c99
 LDFLAGS = -lpthread
 
 all: proxy
--- proxy.c
+++ proxy.c
@@ -9,11 +9,12 @@ void fetchRequest(int connfd);
 
 int main (int argc, char *argv []){
 	
-	int listenfd, connfd, port, clientlen;
+	int listenfd, connfd, port;
+    socklen_t clientlen;
 	struct sockaddr_in clientaddr;
 	
 	if(argc != 2){
-		fprintf(stderr,"ussage %s <port>\n",argv[0]);
+		fprintf(stderr,"Usage %s <port>\n",argv[0]);
 		exit(1);
 	}
 	port = atoi(argv[1]);
@@ -21,12 +22,24 @@ int main (int argc, char *argv []){
 	while(1) {
 		clientlen = sizeof(clientaddr);
 		connfd = Accept(listenfd , (SA *)&clientaddr, &clientlen);
+        printf("Accepted connection\n");
 		fetchRequest(connfd);
 		Close(connfd);
-}
-	
-void fetchRequest(int connfd){
-	size_t n;
-	char buf
+   }
 }
 
+void fetchRequest(int connfd){
+    //@TODO: something useful
+
+    char buffer[MAXLINE];
+    rio_t rio;
+    Rio_readinitb(&rio, connfd);
+    while(Rio_readlineb(&rio, buffer, MAXLINE))
+    {
+        printf("Server got:\n\t%s", buffer);
+        if(buffer[0] == EOF)
+        {
+            return;
+        }
+    }
+}
